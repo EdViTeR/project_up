4400e47c875cd02c198d7094d67c22f660c9b408
EdViTeR <maxpivnenko@mail.ru>
2026-05-27 17:38:04 +0300

    first commit

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>О нас</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		Это основное содержимое страницы "О нас".
+		<br>
+		<a href="index.php">Главная</a>
+		<br>
+		<a href="about.php">О нас</a>
+		<br>
+		<a href="sign_in.php">Войти</a>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Это заголовок тайтл</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		Это основное содержимое страницы.
+		<br>
+		<a href="about.php">О нас</a>
+		<br>
+		<a href="sign_in.php">Войти</a>
+	</body>
+</html>
